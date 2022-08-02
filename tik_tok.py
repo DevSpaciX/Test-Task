6bb36d1114a5ebc206c801f22a9f4af8347cd700
SpaciX <winofkill@gmail.com>
2022-08-02 10:48:46 +0400

    final

--- tik_tok.py
+++ tik_tok.py
@@ -1,9 +1,7 @@
 from moviepy.editor import VideoFileClip
 import requests
 import os
-video_url = 'https://samplelib.com/lib/preview/mp4/sample-5s.mp4'
-#Your video url unfourchantly don`t working
-# https://v16m-webapp.tiktokcdn-us.com/ed129ecb01ab00e202682e99f68a9288/62e7cb0d/video/tos/useast5/tos-useast5-pve-0068-tx/d69985b1677b4a73a584b56d604011ca/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=4020&bt=2010&cs=0&ds=3&ft=ebtHKH-qMyq8ZjFl1we2N9befl7Gb&mime_type=video_mp4&qs=0&rc=OTU4MzU0NzVnaDpnOGg8OEBpajM5Z2c6ZmYzZTMzZzczNEAuMC9jLWBgNmExMzJfY18tYSMxX28vcjRnMGRgLS1kMS9zcw%3D%3D&l=20220801064449EF653E99EF32BC2EAB55
+video_url = 'https://v16m-webapp.tiktokcdn-us.com/a6e3b7e6a064bc43da3a39ab93460ef8/62e921d5/video/tos/useast5/tos-useast5-ve-0068c003-tx/41b44c2a3cc840929f658665d57979d1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1150&bt=575&cs=0&ds=3&ft=ebtHKH-qMyq8ZwkJKwe2NoTcfl7Gb&mime_type=video_mp4&qs=0&rc=aTozOTNmZmg5MzpnOzo1ZUBpM3g6OmQ6ZmhvZTMzZzczNEAwLjRiMDZhNmMxLmJfMTYzYSNqczAycjRnYmZgLS1kMS9zcw%3D%3D&l=20220802070706CBA8CD072C869B2562D9'
 
 def download_video(url):
     try:
@@ -17,12 +15,19 @@ def download_video(url):
 
 def main():
     print(download_video(url=video_url))
-
 if __name__ == '__main__':
     main()
+def convert():
+    clip = VideoFileClip('TikTok-example-1.mp4')
+    clip.write_gif('TikTok-example-1.gif')
 
-clip = VideoFileClip('TikTok-example-1.mp4')
-clip.write_gif('TikTok-example-1.gif')
+convert()
 print(os.path.abspath('TikTok-example-1.gif'))
 
-#probably we need to remove mp4 file
+def delete():
+    try:
+        os.remove('TikTok-example-1.mp4')
+    except Exception as ex:
+        return
+delete()
+    #probably we need to remove mp4 file
